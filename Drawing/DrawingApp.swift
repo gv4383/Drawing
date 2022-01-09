21e6049c43853b97ddbbeffd2a25c9c73144f3be
gv4383 <gregory.vargas18@gmail.com>
2022-01-08 19:02:12 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Goyo Vargas on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
