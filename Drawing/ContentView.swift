d73e6b6cde1e8af8beffbcc26e8d788f5a3a5e42
gv4383 <gregory.vargas18@gmail.com>
2022-01-09 16:04:27 -0600

    Creating custom paths

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,10 +7,19 @@
 
 import SwiftUI
 
+// Creating custom paths
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+
+//            // not needed if you use StrokeStyle API
+//            path.closeSubpath()
+        }
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
