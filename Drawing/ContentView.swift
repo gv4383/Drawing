a3730cd226dfbeca438b7c6eb49a0ab63cb0a87f
gv4383 <gregory.vargas18@gmail.com>
2022-01-10 09:46:55 -0600

    Transforming shapes using CGAffineTransform and even-odd fills

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -73,46 +73,89 @@ import SwiftUI
 //}
 
 // Part 3 - Adding strokeBorder() support with InsettableShape
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockwise: Bool
-    var insetAmount = 0.0
+//struct Arc: InsettableShape {
+//    let startAngle: Angle
+//    let endAngle: Angle
+//    let clockwise: Bool
+//    var insetAmount = 0.0
+//
+//    func path(in rect: CGRect) -> Path {
+//        let rotationAdjustment = Angle.degrees(90)
+//        let modifiedStart = startAngle - rotationAdjustment
+//        let modifiedEnd = endAngle - rotationAdjustment
+//
+//        var path = Path()
+//
+//        path.addArc(
+//            center: CGPoint(x: rect.midX, y: rect.midY),
+//            radius: rect.width / 2 - insetAmount,
+//            startAngle: modifiedStart,
+//            endAngle: modifiedEnd,
+//            clockwise: !clockwise
+//        )
+//
+//        return path
+//    }
+//
+//    func inset(by amount: CGFloat) -> some InsettableShape {
+//        var arc = self
+//        arc.insetAmount += amount
+//        return arc
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+////        Circle()
+////            .stroke(.blue, lineWidth: 40)
+////            .strokeBorder(.blue, lineWidth: 40)
+//        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+//            .strokeBorder(.blue, lineWidth: 40)
+//    }
+//}
 
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
-        var path = Path()
-
-        path.addArc(
-            center: CGPoint(x: rect.midX, y: rect.midY),
-            radius: rect.width / 2 - insetAmount,
-            startAngle: modifiedStart,
-            endAngle: modifiedEnd,
-            clockwise: !clockwise
-        )
-
-        return path
-    }
+// Part 4 - Transforming shapes using CGAffineTransform and even-odd fills
+struct Flower: Shape {
+    var petalOffset = -20.0
+    var petalWidth = 100.0
     
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
     }
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-//        Circle()
-//            .stroke(.blue, lineWidth: 40)
-//            .strokeBorder(.blue, lineWidth: 40)
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//                .stroke(.red, lineWidth: 1)
+                .fill(.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
