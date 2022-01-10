7c1e98970d0f4481a7bd9a869397184963936fce
gv4383 <gregory.vargas18@gmail.com>
2022-01-09 23:17:42 -0600

    Adding strokeBorder() support with InsettableShape

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -24,23 +24,60 @@ import SwiftUI
 //}
 
 // Part 2 - Paths vs shapes
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
+//struct Triangle: Shape {
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//
+//        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+//        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+//
+//        return path
+//    }
+//}
+//
+//struct Arc: Shape {
+//    let startAngle: Angle
+//    let endAngle: Angle
+//    let clockwise: Bool
+//
+//    func path(in rect: CGRect) -> Path {
+//        let rotationAdjustment = Angle.degrees(90)
+//        let modifiedStart = startAngle - rotationAdjustment
+//        let modifiedEnd = endAngle - rotationAdjustment
+//
+//        var path = Path()
+//
+//        path.addArc(
+//            center: CGPoint(x: rect.midX, y: rect.midY),
+//            radius: rect.width / 2,
+//            startAngle: modifiedStart,
+//            endAngle: modifiedEnd,
+//            clockwise: !clockwise
+//        )
+//
+//        return path
+//    }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+////        Triangle()
+////            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+////            .frame(width: 300, height: 300)
+//        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+//            .stroke(.blue, lineWidth: 10)
+//            .frame(width: 300, height: 300)
+//    }
+//}
 
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
-    }
-}
-
-struct Arc: Shape {
+// Part 3 - Adding strokeBorder() support with InsettableShape
+struct Arc: InsettableShape {
     let startAngle: Angle
     let endAngle: Angle
     let clockwise: Bool
+    var insetAmount = 0.0
 
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
@@ -51,7 +88,7 @@ struct Arc: Shape {
 
         path.addArc(
             center: CGPoint(x: rect.midX, y: rect.midY),
-            radius: rect.width / 2,
+            radius: rect.width / 2 - insetAmount,
             startAngle: modifiedStart,
             endAngle: modifiedEnd,
             clockwise: !clockwise
@@ -59,16 +96,21 @@ struct Arc: Shape {
 
         return path
     }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-//        Triangle()
-//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-//            .frame(width: 300, height: 300)
-        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-            .stroke(.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
+//        Circle()
+//            .stroke(.blue, lineWidth: 40)
+//            .strokeBorder(.blue, lineWidth: 40)
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
     }
 }
 
